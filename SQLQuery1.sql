44ee16a7b310c4f958f69069e19a79b37f076f0b
KidoKosho <152004626+KidoKosho@users.noreply.github.com>
2025-10-24 13:58:11 +0700

    push sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+SELECT * FROM thvComputers
+Insert into thvComputers(thvComputersId,thvComputersName,thvComPrice,thvComImgae,thvComStatus) values
+('1','Dell1',1000000,'https://minhancomputer.com/media/lib/09-01-2021/hpenvy.jpg',1),
+('2','Dell1',1000000,'https://minhancomputer.com/media/lib/09-01-2021/hpenvy.jpg',1),
+('3','Dell2',1000000,'https://minhancomputer.com/media/lib/09-01-2021/hpenvy.jpg',0);
